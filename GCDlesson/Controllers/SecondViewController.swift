42fa7cb0a5a679e862347be0186c32ccbfff9774
Anastasiya Ulitina <anastulitina@gmail.com>
2020-11-21 15:27:45 +0300

    delay implement

--- GCDlesson/Controllers/SecondViewController.swift
+++ GCDlesson/Controllers/SecondViewController.swift
@@ -28,15 +28,48 @@ class SecondViewController: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchImage()
-
+        delay(3) {
+            self.loginAlert()
+        }
         // Do any additional setup after loading the view.
     }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping() -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
+    
+    //MARK: - Login Alert method
+    
+    fileprivate func loginAlert() {
+        let ac = UIAlertController(title: "Зарегистрироаны?", message: "Введите логин и пароль", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ac.addAction(okAction)
+        ac.addAction(cancelAction)
+        
+        ac.addTextField { (userLoginTF) in
+            userLoginTF.placeholder = "Login"
+        }
+        ac.addTextField { (userPasswordTF) in
+            userPasswordTF.placeholder = "Password"
+            userPasswordTF.isSecureTextEntry = true
+        }
+        
+        self.present(ac, animated: true, completion: nil)
+    }
+    
+    //MARK: - Fetch image method
     
     fileprivate func fetchImage() {
         activityIndicator.startAnimating()
         imageURL = URL(string: "https://i.pinimg.com/564x/31/7e/fa/317efa7c26404b7f2c316e68081776f3.jpg")
-        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
-        self.image = UIImage(data: imageData)
+        DispatchQueue.main.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            self.image = UIImage(data: imageData)
+        }
+        
     }
     
     
